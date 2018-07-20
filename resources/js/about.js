aef7fb7e9344fa440863059d45af157c56ca53c5
Tyler Norris <tyjnorris@gmail.com>
2018-07-20 10:18:17 -0400

    banner updated and nav bar changed

---
+++ resources/js/about.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('html,body').animate({
+  scrollTop: $(window.location.hash).offset().top
+});
+});
---
+++ resources/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+$(".page-navigation .item a").on("click", function(event){
+  debugger
+  event.preventDefault();
+  var headerHeight = $("header").height();
+  var idname = $(this).attr("href");
+  var originalLocation = $(idname).offset().top;
+  var newLocation = originalLocation - headerHeight;
+  $("html").animate({
+    scrollTop : newLocation
+  });
+});
+    $(".regular").slick({
+          dots: true,
+          infinite: true,
+          arrows: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+      });
+
+});
